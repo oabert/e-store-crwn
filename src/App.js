cf22814da2a7c80ac3e1dd14ac1c64c528f4c1e4
Olena Abert <oabert@gmail.com>
2021-11-02 21:56:12 -0400

    add collection-item and collection-preview components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import React from 'react';
 import {Route} from 'react-router-dom';
-import HatsPageComponent from "./pages/hats-page/hats-page.component";
+import ShopPageComponent from "./pages/shop-page/shop-page.component";
+// import HatsPageComponent from "./pages/hats-page/hats-page.component";
 
 function App() {
   return (
     <div>
     <Route exact path='/' component={Homepage}/>
-    <Route path='/:hats' component={HatsPageComponent}/>
+    <Route path='/shop' component={ShopPageComponent}/>
     </div>
   );
 }
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './collection-preview.style.scss'
+import CollectionItemComponent from "../collection-items/collection-item.component.jsx";
+
+const CollectionPreviewComponent = ({title, items}) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...otherCollectionProps}) => (
+                    <CollectionItemComponent key={id} {...otherCollectionProps}/>
+                ))
+            }
+        </div>
+    </div>
+);
+
+export default CollectionPreviewComponent;
