047ae5bececea0da81bbb59e623faa9ebe91b5f0
Olena Abert <oabert@gmail.com>
2022-01-11 16:31:19 -0500

    add remove item from cart func.,add increase and decrease quantity of item in cart func.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,15 @@
 import cartActionTypes from './cart.types';
-import {addItemToCart} from "./cart.utils";
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
-const INITIAL_STATE={
+const INITIAL_STATE = {
     hidden: true,
     cartItems: []
 };
 
-const cartReducer = (state = INITIAL_STATE, action)=>{
-    switch (action.type){
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
         case cartActionTypes.TOGGLE_CART_HIDDEN:
-            return{
+            return {
                 ...state,
                 hidden: !state.hidden
             };
@@ -17,6 +17,18 @@ const cartReducer = (state = INITIAL_STATE, action)=>{
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case cartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case cartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
             }
         default:
             return state;
